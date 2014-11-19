17e9a211d9ec50bd7360ac317b82b1c8c7529ede
Ben Alexander <balexand@broadinstitute.org>
2014-11-19 12:54:26 -0500

    remove redirect from URL mappings.  Add build info to events on war building, and a placeholder during compile.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,8 +6,11 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-       // "/"(redirect:[controller: 'home', action: 'portalHome',permanent:true])
-        "/"(redirect:[controller: 'home', action: 'index',permanent:true])
+//        "/"(redirect:[controller: 'home', action: 'index',permanent:true])
+        "/"{
+            controller = "home"
+            action = "index"
+        }
         "500"(view:'/error')
 	}
 }
