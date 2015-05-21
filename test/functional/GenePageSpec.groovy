3861b48a473881a1991a8d4a404f883e5d66e849
andrew <andrew@broadinstitute.org>
2015-05-21 11:32:10 -0400

    Test now waits for all browser windows to login to google and then hits the gene page all at once.  Added some robustness improvements, expanded the size of the gene list.

--- test/functional/GenePageSpec.groovy
+++ test/functional/GenePageSpec.groovy
@@ -4,8 +4,10 @@ import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.openqa.selenium.By
+import org.openqa.selenium.SessionNotCreatedException
 import org.openqa.selenium.TimeoutException
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebDriverException
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.remote.DesiredCapabilities
@@ -27,10 +29,11 @@ import java.text.SimpleDateFormat
 class GenePageSpec {
 
     // 1. download and install chromedriver  https://sites.google.com/a/chromium.org/chromedriver/downloads.  Set -Dwebdriver.chrome.driver to the path to the executable.
-    // 2. set -Dt2dportal.username and -Dt2dportal.password to the account you want to login with,
+    // 2. set -Dt2dportal.username and -Dt2dportal.password to the account you want to login with
     // 3. DO NOT put username/password under source control
-    // 4. adjust the number of concurrent threads by doing a sublist of the gene names
-    // 5. if you end up with lots of chrome windows, run this little number
+    // 4. up the default memory settings: -XX:MaxPermSize=1024m -Xmx2g
+    // 5. adjust the number of concurrent threads by doing a sublist of the gene names
+    // 6. if you end up with lots of chrome windows, run this little number
     // to kill the processes: kill $(ps aux | grep -i '\-\-test-type=webdriver' | awk '{print $2}')
 
     private static WebDriver driver;
@@ -39,12 +42,20 @@ class GenePageSpec {
 
     private List<String> passingTestSummaries = new ArrayList<>();
 
+    private static final Map<String,Boolean> loginForGene = new HashMap<>();
+
     @Test
     public void loadTestGenesPage() {
 
         Collection<String> genes = getGeneNames();
         Collections.shuffle(genes);
-        for (String gene : genes[0..15]) {
+        Collection<String> genesToTest = genes[0..32];
+
+        for (String gene : genesToTest) {
+            loginForGene.put(gene,false);
+        }
+
+        for (String gene : genesToTest) {
             startGeneNameSearchThread(gene);
         }
 
@@ -72,17 +83,28 @@ class GenePageSpec {
     }
 
     /**
-     * Spawns a new browser window, logs in via google,
-     * and loads the gene page for the given gene.
+     * Logs into the app.  This is serialized so that only one browser
+     * can login at a time in order to:
+     * 1. minimize the chance of login redirects flaking out
+     * 2. ensuring that the calls to load the gene page happen
+     * at mostly the same time
      */
-    private void searchForGeneName(String geneName) {
+    private static synchronized void login(WebDriver driver) {
         String username = System.getProperty("t2dportal.username");
         String password = System.getProperty("t2dportal.password");
+        try {
+            driver.get("http://type2diabetesgen-qasrvr.elasticbeanstalk.com/gene/geneInfo/");
+        }
+        catch(WebDriverException e) {
+            System.err.println("Attempt to load initial page failed, retrying...");
+            try {
+                Thread.sleep(5000);
+            }
+            catch(InterruptedException ie){}
+            driver.get("http://type2diabetesgen-qasrvr.elasticbeanstalk.com/gene/geneInfo/");
 
-
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://type2diabetesgen-qasrvr.elasticbeanstalk.com/gene/geneInfo/" + geneName);
-
+        }
+        Thread.sleep(1000);
         WebElement googleLoginButton = driver.findElementByClassName("googleLoginButton");
 
         long signInStartTime = -1;
@@ -98,22 +120,69 @@ class GenePageSpec {
             println("Already logged in?")
         }
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(driver, 20);
         try {
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("T2D-GENES consortium")));
         }
         catch(TimeoutException e) {
-            println(geneName + ": GOOGLE LOGIN FAILED");
+            println(": GOOGLE LOGIN FAILED");
             return;
         }
-        println("It took " + (System.currentTimeMillis() - signInStartTime) + "ms to sign in via google for " + geneName);
+        println("It took " + (System.currentTimeMillis() - signInStartTime) + "ms to sign in via google.");
+    }
+
+    /**
+     * Returns true once all browser windows have logged in.
+     */
+    private static boolean allGenesLoggedIn() {
+        return getNumGenesLoggedIn() == loginForGene.size();
+    }
+
+    private static int getNumGenesLoggedIn() {
+        int numLoggedIn = 0;
+        for (Map.Entry<String, Boolean> geneLoggedIn : loginForGene.entrySet()) {
+            if (geneLoggedIn.getValue()) {
+                numLoggedIn++;
+            }
+        }
+        return numLoggedIn;
+    }
+
+    /**
+     * Spawns a new browser window, logs in via google,
+     * and loads the gene page for the given gene.
+     */
+    private void searchForGeneName(String geneName) {
+        WebDriver driver = null;
+
+        try {
+            driver = new ChromeDriver()
+        }
+        catch(SessionNotCreatedException e) {
+            System.err.println("Failed to create new WebDriver, trying again...")
+            try {
+                Thread.sleep(5000);
+            }
+            catch(InterruptedException ie) {}
+            driver = new ChromeDriver();
+        }
+        login(driver);
+        loginForGene.put(geneName,true);
+        println(getNumGenesLoggedIn() + " of " + loginForGene.size() + " logins are complete.");
+
+        while (!allGenesLoggedIn()) {
+            try {
+                Thread.sleep(500)
+            }
+            catch(InterruptedException e) {}
+        }
 
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS")
         println("Querying " + geneName + " at " + dateFormat.format(System.currentTimeMillis()));
         long startTime = System.currentTimeMillis();
         driver.get("http://type2diabetesgen-qasrvr.elasticbeanstalk.com/gene/geneInfo/" + geneName);
 
-        wait = new WebDriverWait(driver, 60);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
         try {
             wait.until(new ExpectedCondition<Boolean>() {
                 @Override
@@ -136,6 +205,21 @@ class GenePageSpec {
     private String[] getGeneNames() {
         return [
                 "HIST1H1D",
+                "TRRAP",
+                "DNPEP",
+                "CDO1",
+                "MIF4GD",
+                "FRK",
+                "HIC2",
+                "LRWD1",
+                "BCAN",
+                "PLEKHG3",
+                "VPS33B",
+                "GRAMD3",
+                "RRP12",
+                "SERINC1",
+                "MYO1F",
+                "KIAA2018",
                 "MDC1",
                 "C20orf141",
                 "MAP4",
