b9f88c3b19552984fb219f17ebb1744e07c7535e
andrew <andrew@broadinstitute.org>
2015-06-02 14:39:19 -0400

    Sketch of metadata taglib

---
+++ grails-app/taglib/dport/MetadataTagLib.groovy
@@ -0,0 +1,13 @@
+package dport
+
+class MetadataTagLib {
+    static defaultEncodeAs = [taglib: 'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+    SharedToolsService sharedToolsService
+
+    def queryMetadata = { attrs,body ->
+        // retrieve metadata
+
+    }
+}
