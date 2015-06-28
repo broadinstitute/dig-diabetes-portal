87fe101380f62d033b0db7c2fa06679cf3c8a511
andrew <andrew@broadinstitute.org>
2015-06-28 14:29:26 -0400

    Added sigma controller and web request service for looking up english/spanish.

---
+++ grails-app/services/dport/WebRequestLanguageLookupService.groovy
@@ -0,0 +1,13 @@
+package dport
+
+import grails.transaction.Transactional
+import org.codehaus.groovy.grails.web.util.WebUtils
+import org.springframework.web.servlet.support.RequestContextUtils
+
+@Transactional
+class WebRequestLanguageLookupService {
+
+    def getLanguageForCurrentWebRequest() {
+        return RequestContextUtils.getLocale(WebUtils.retrieveGrailsWebRequest().currentRequest).toLanguageTag();
+    }
+}
