d7b329ca3e76f49f9f2e7e9602aec51e445451ee
mduby <mduby@broadinstitute.org>
2015-08-10 11:10:52 -0400

    adding metadata parsing unit tests

---
+++ src/java/org/broadinstitute/mpg/diabetes/util/PortalException.java
@@ -0,0 +1,11 @@
+package org.broadinstitute.mpg.diabetes.util;
+
+/**
+ * Custom exception class fo the portal project
+ *
+ */
+public class PortalException extends Exception {
+    public PortalException(String message) {
+        super(message);
+    }
+}
