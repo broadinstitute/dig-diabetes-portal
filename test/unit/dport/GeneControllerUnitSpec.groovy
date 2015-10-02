d0c8a3031f1e95b77f11dbef6e2c8f8bd3ba7a4c
mduby <mduby@broadinstitute.org>
2015-10-02 16:11:17 -0400

    DIGP-112: fixed unit test failure due to new service reference in the controller class

--- test/unit/dport/GeneControllerUnitSpec.groovy
+++ test/unit/dport/GeneControllerUnitSpec.groovy
@@ -1,21 +1,30 @@
 package dport
-
-import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
+import org.broadinstitute.mpg.diabetes.MetaDataService
 import spock.lang.Specification
-
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
+
 @TestMixin(GrailsUnitTestMixin)
 @TestFor(GeneController)
 ///@Mock([Shoppable, QueryItem, CartAssay, CartProject])
 class GeneControllerUnitSpec extends Specification {
     SharedToolsService sharedToolsService = new  SharedToolsService()
     GeneManagementService geneManagementService = new GeneManagementService()
+    def metaDataService = new MetaDataService()
     RestServerService restServerService = new RestServerService()
+
+    def setupSpec() {
+        MetaDataService.metaClass.urlEncodedListOfPhenotypes = { ->
+            // do something here
+        }
+        GeneManagementService.metaClass.getRegionSpecificationForGene = { ->
+            // do something here
+        }
+    }
 
     def setup() {
     }
@@ -27,6 +36,7 @@ class GeneControllerUnitSpec extends Specification {
         setup:
         controller.geneManagementService = geneManagementService
         controller.sharedToolsService = sharedToolsService
+        controller.metaDataService = metaDataService
 
         when:
          controller.geneInfo()
