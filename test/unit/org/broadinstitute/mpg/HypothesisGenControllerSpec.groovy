1117694c1a0c2cc010ddce92008bd4bfb3005e2a
mduby <mduby@broadinstitute.org>
2015-10-22 17:05:59 -0400

    DIGP-161: Moved HypothesisGenController to new package

---
+++ test/unit/org/broadinstitute/mpg/HypothesisGenControllerSpec.groovy
@@ -0,0 +1,18 @@
+package org.broadinstitute.mpg
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(HypothesisGenController)
+class HypothesisGenControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
