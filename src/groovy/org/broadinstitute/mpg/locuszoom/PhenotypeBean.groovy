81323ca26c99c41ea46e8ccbcfaa828752b3b51c
mduby <mduby@broadinstitute.org>
2016-05-24 16:14:59 -0400

    adding LZ phenotype map

---
+++ src/groovy/org/broadinstitute/mpg/locuszoom/PhenotypeBean.groovy
@@ -0,0 +1,13 @@
+package org.broadinstitute.mpg.locuszoom
+
+/**
+ * Bean class to encapsulate phenotype information for the LZ dropdown selection and LZ display withing the plot
+ *
+ */
+public class PhenotypeBean {
+    String key;
+    String displayName;
+    String name;
+    String description;
+    Boolean defaultSelected
+}
