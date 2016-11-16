bd3373ed3452bfeffee58335c67dbb413ead1218
Ben R. Alexander <balexand@broadinstitute.org>
2016-11-16 10:01:57 -0500

    Create a version to help debug IGV.

--- grails-app/conf/portalResources.groovy
+++ grails-app/conf/portalResources.groovy
@@ -111,8 +111,11 @@ modules = {
         resource url: 'images/ajaxLoadingAnimation.gif'
         resource url: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'
 
-        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.css'
-        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.min.js'
+//        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.css'
+//        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.min.js'
+        resource url: 'https://igv.org/web/release/1.0.5/igv-1.0.5.css'
+        resource url: 'https://igv.org/web/release/1.0.5/igv-1.0.5.js'
+
     }
     bootstrapMultiselect {
         resource url: 'js/lib/bootstrap-multiselect.js'
@@ -148,8 +151,10 @@ modules = {
     }
     igvNarrow {  // IGV on a page with core
         resource url: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'
-        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.css'
-        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.min.js'
+//        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.css'
+//        resource url: 'https://igv.org/web/release/1.0.1/igv-1.0.1.min.js'
+        resource url: 'https://igv.org/web/release/1.0.5/igv-1.0.5.css'
+        resource url: 'https://igv.org/web/release/1.0.5/igv-1.0.5.js'
     }
     sigma {  // sigma site
         resource url: 'css/dport/sigma.css'
