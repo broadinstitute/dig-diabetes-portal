2af797bbbfcf24a9d96f3f210f1f1818047bf57b
Ben Alexander <balexand@broadinstitute.org>
2018-05-07 13:05:53 -0400

    make sure that the default get agg call for stroke is also set to the newest API

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -624,7 +624,7 @@ portal.data.versionDesignator = [ new PortalVersionBean("t2d",      // label for
                                           1,
                                           0,
                                           0,
-                                          0,
+                                          1,
                                           0,
                                           0,0,0
                                   ),
