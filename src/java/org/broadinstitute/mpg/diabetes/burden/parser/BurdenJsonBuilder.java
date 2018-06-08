44f59831f06e5ad4c2a6383cebe776ae09420f4d
Ben Alexander <balexand@broadinstitute.org>
2018-06-08 10:58:29 -0400

    remove limit on page size for gene burden page

--- src/java/org/broadinstitute/mpg/diabetes/burden/parser/BurdenJsonBuilder.java
+++ src/java/org/broadinstitute/mpg/diabetes/burden/parser/BurdenJsonBuilder.java
@@ -246,8 +246,8 @@ public class BurdenJsonBuilder {
         }
 
         // get the payload string
-        query.setPageSize(5000);
-        query.setLimit(5000);
+        query.setPageSize(-1);
+        query.setLimit(-1);
         //query.setPageSize(1000);
         //query.setLimit(1000);
         jsonString = jsonBuilder.getQueryJsonPayloadString(query);
