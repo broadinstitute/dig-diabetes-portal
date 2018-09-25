932f881b5faeb51eedfaa2e65ae1f9e9f1ecb104
mariacos <mariacos@broadinstitute.org>
2018-09-25 13:21:15 -0400

    editing new dataset flags

--- web-app/js/lib/dport/datasetsPage.js
+++ web-app/js/lib/dport/datasetsPage.js
@@ -295,7 +295,7 @@ var mpgSoftware = mpgSoftware || {};
                 $("#phenotypeFilterLevel1Display").empty().append(filterDynamicHtml);
                 renderFilteredData();
 
-                var newDatasets = ["DIAMANTE"];
+                var newDatasets = ["GoDarts", "Hoorn", "AAGILE", "Diabetic Kidney Disease"];
                 addNewDatasetFlag (newDatasets,".accordion-toggle","<span class='new-dataset-flag'>&nbsp;</span>"); //[DK] This function exists on t2dGenesCore.gsp. It's called to add "New dataset Flag
 
                 return {
