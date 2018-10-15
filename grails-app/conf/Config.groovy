74d129d2cddccf2bdf39f6cab6c5b618f77d0c75
mariacos <mariacos@broadinstitute.org>
2018-10-15 15:21:10 -0400

    expose forest plot on sleep portal

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -829,7 +829,7 @@ portal.data.versionDesignator = [ new PortalVersionBean("t2d",      // label for
                                           0,
                                           1,
                                           0,
-                                          1,0,1,
+                                          1,1,1,
                                           0,
                                           0,
                                           0,
