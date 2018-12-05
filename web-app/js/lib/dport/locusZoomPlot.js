2386d34d6002148c166c0f57e966e8b4e4c35e88
Ben Alexander <balexand@broadinstitute.org>
2018-12-05 15:04:05 -0500

    fix a bug which caused the Include all datasets option on the pheWAS plot to fail

--- web-app/js/lib/dport/locusZoomPlot.js
+++ web-app/js/lib/dport/locusZoomPlot.js
@@ -1786,21 +1786,25 @@ var mpgSoftware = mpgSoftware || {};
                     if ($('#phewasAllDatasets').is(":checked")){
                         includeAllDatasets=true;
                     }
+                    var includeAllDatasetsRequest = "false";
+                    if (includeAllDatasets){
+                        includeAllDatasetsRequest = "true";
+                    }
                     var urlForPhewas = pageVars.phewasAjaxCallInLzFormatUrl;
                     if ($('#phewasUseUKBB').is(":checked")) {
                         //urlForPhewas = 'http://portaldev.sph.umich.edu/ukbb/v1/statistic/phewas/?filter=variant%20eq%20%277:90350840_C/T%27&format=objects&build=GRCh37';
                         urlForPhewas = encodeURI('http://portaldev.sph.umich.edu/ukbb/v1/statistic/phewas/?filter=variant eq \''+convertVarIdToUmichFavoredForm(variantIdString)+'\''+
                         '&format=objects&build=GRCh37');
+                        includeAllDatasetsRequest="GRCh37";
                     }
-                    var includeAllDatasetsRequest = "false";
-                    if (includeAllDatasets){
-                        includeAllDatasetsRequest = "true";
-                    }
+
                     ds
                         .add("phewas", ["PheWASLZ", {
                             url: urlForPhewas,
-                            params: { build: ["GRCh37"],
-                                includeAllVariants:[includeAllDatasetsRequest]}
+                            params: { build: [includeAllDatasetsRequest],
+                                includeAllVariants:[includeAllDatasetsRequest]} //work around.  we don't see this
+                                                                                // parm in Grails phewasAjaxCallInLzFormat
+                                                                                // we should.  for now, pass data in build
                         }]);
                         console.log('selector='+selector+'.');
                         lzp = LocusZoom.populate(selector, ds, newLayout);
