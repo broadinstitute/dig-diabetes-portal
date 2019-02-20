5fc86a7dc41ce8e97d9e51c935d3c28a1900121b
Ben R. Alexander <balexand@broadinstitute.org>
2019-02-19 19:46:46 -0500

    Add sorting capabilities, though something is not working yet

--- web-app/js/lib/dport/dynamicUi.js
+++ web-app/js/lib/dport/dynamicUi.js
@@ -13,6 +13,155 @@ var mpgSoftware = mpgSoftware || {};
  * @type {{installDirectorButtonsOnTabs, modifyScreenFields, adjustLowerExtent, adjustUpperExtent}}
  */
 
+(function( factory ){
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD
+        define( ['jquery', 'datatables.net'], function ( $ ) {
+            return factory( $, window, document );
+        } );
+    }
+    else if ( typeof exports === 'object' ) {
+        // CommonJS
+        module.exports = function (root, $) {
+            if ( ! root ) {
+                root = window;
+            }
+
+            if ( ! $ || ! $.fn.dataTable ) {
+                $ = require('datatables.net')(root, $).$;
+            }
+
+            return factory( $, root, root.document );
+        };
+    }
+    else {
+        // Browser
+        factory( jQuery, window, document );
+    }
+}(function( $, window, document, undefined ) {
+    'use strict';
+
+// Unique value allowing multiple absolute ordering use cases on a single page.
+    var _unique = 0;
+
+// Function to encapsulate code that is common to both the string and number
+// ordering plug-ins.
+    var _setup = function ( values ) {
+        if ( ! $.isArray( values ) ) {
+            values = [ values ];
+        }
+
+        var o = {
+            name: 'absoluteOrder'+(_unique++),
+            alwaysTop: {},
+            alwaysBottom: {}
+        };
+
+        // In order to provide performance, the symbols that are to be looked for
+        // are stored as parameter keys in an object, allowing O(1) lookup, rather
+        // than O(n) if it were in an array.
+        for ( var i=0, ien=values.length ; i<ien ; i++ ) {
+            var conf = values[i];
+
+            if ( typeof conf === 'string' ) {
+                o.alwaysTop[ conf ] = true;
+            }
+            else if ( conf.position === undefined || conf.position === 'top' ) {
+                o.alwaysTop[ conf.value ] = true;
+            }
+            else {
+                o.alwaysBottom[ conf.value ] = true;
+            }
+        }
+
+        // Ascending ordering method
+        o.asc = function ( a, b, isNumber ) {
+            if ( o.alwaysTop[ a ] && o.alwaysTop[ b ] ) {
+                return 0;
+            }
+            else if ( o.alwaysBottom[ a ] && o.alwaysBottom[ b ] ) {
+                return 0;
+            }
+            else if ( o.alwaysTop[ a ] || o.alwaysBottom[ b ] ) {
+                return -1;
+            }
+            else if ( o.alwaysBottom[ a ] || o.alwaysTop[ b ] ) {
+                return 1;
+            }
+
+            if ( isNumber ) {
+                // Cast as a number if required
+                if ( typeof a === 'string' ) {
+                    a = a.replace(/[^\d\-\.]/g, '') * 1;
+                }
+                if ( typeof b === 'string' ) {
+                    b = b.replace(/[^\d\-\.]/g, '') * 1;
+                }
+            }
+
+            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+        };
+
+        // Descending ordering method
+        o.desc = function ( a, b, isNumber ) {
+            if ( o.alwaysTop[ a ] && o.alwaysTop[ b ] ) {
+                return 0;
+            }
+            else if ( o.alwaysBottom[ a ] && o.alwaysBottom[ b ] ) {
+                return 0;
+            }
+            else if ( o.alwaysTop[ a ] || o.alwaysBottom[ b ] ) {
+                return -1;
+            }
+            else if ( o.alwaysBottom[ a ] || o.alwaysTop[ b ] ) {
+                return 1;
+            }
+
+            if ( isNumber ) {
+                if ( typeof a === 'string' ) {
+                    a = a.replace(/[^\d\-\.]/g, '') * 1;
+                }
+                if ( typeof b === 'string' ) {
+                    b = b.replace(/[^\d\-\.]/g, '') * 1;
+                }
+            }
+
+            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+        };
+
+        return o;
+    };
+
+// String based ordering
+    $.fn.dataTable.absoluteOrder = function ( values ) {
+        var conf = _setup( values );
+
+        $.fn.dataTable.ext.type.order[ conf.name+'-asc' ] = conf.asc;
+        $.fn.dataTable.ext.type.order[ conf.name+'-desc' ] = conf.desc;
+
+        // Return the name of the sorting plug-in that was created so it can be used
+        // with the `columns.type` parameter. There is no auto-detection here.
+        return conf.name;
+    };
+
+// Number based ordering - strips out everything but the number information
+    $.fn.dataTable.absoluteOrderNumber = function ( values ) {
+        var conf = _setup( values );
+
+        $.fn.dataTable.ext.type.order[ conf.name+'-asc' ] = function ( a, b ) {
+            return conf.asc( a, b, true );
+        };
+        $.fn.dataTable.ext.type.order[ conf.name+'-desc' ] = function ( a, b ) {
+            return conf.desc( a, b, true );
+        };
+
+        return conf.name;
+    };
+
+
+}));
+
+
 
 mpgSoftware.dynamicUi = (function () {
     var loading = $('#rSpinner');
@@ -2978,6 +3127,23 @@ mpgSoftware.dynamicUi = (function () {
     }
 
 
+
+
+
+
+
+
+    var namesType = $.fn.dataTable.absoluteOrder( [
+        { value: 'Artery_Aorta', position: 'top' }
+    ] );
+
+    var numbersType = $.fn.dataTable.absoluteOrderNumber( [
+        { value: 'N/A', position: 'bottom' }
+    ] );
+
+
+
+
     /***
      * Initiate the jQuery data table and build its headers. This routine expects objects
      * inside each element of the array which will have
@@ -3030,12 +3196,13 @@ mpgSoftware.dynamicUi = (function () {
                         default:
                             break;
                     }
-                    _.forEach(addedColumns, function (column){
+                    _.forEach(addedColumns, function (column, index){
                         headerDescriber.columnDefs.push({
                             "title": column.content,
-                            "targets": 0,
+                            "targets": index,
                             "name": column.title,
-                            "className": column.annotation
+                            "className": column.annotation,
+                            "type": "absoluteOrder"
                         });
                     });
 
@@ -3049,7 +3216,8 @@ mpgSoftware.dynamicUi = (function () {
                         "title": header.content,
                         "targets": count+numberOfAddedColumns,
                         "name": header.title,
-                        "className": header.annotation
+                        "className": header.annotation,
+                        "type": "absoluteOrder"
                     });
                      addedColumns.push(new IntermediateStructureDataCell(header.title,header.content,header.annotation));
                 });
