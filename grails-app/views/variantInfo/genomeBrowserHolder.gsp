2ee18373021a850fa971b193dd2c1c85d14d3e4e
Ben Alexander <balexand@broadinstitute.org>
2020-01-27 16:00:30 -0500

    adding lots of interesting genomes browser capabilities

--- grails-app/views/variantInfo/genomeBrowserHolder.gsp
+++ grails-app/views/variantInfo/genomeBrowserHolder.gsp
@@ -20,6 +20,88 @@
     <h2>Genome browser</h2>
 </div>
 
+
+
+%{--try example start--}%
+
+
+%{--<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">--}%
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="nav-item dropdown">
+                <a id="igv-example-api-dropdown" href="#" class="dropdown-toggle" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false" style="font-size: 24px; text-decoration: underline">Tracks</a>
+                <ul class="dropdown-menu">
+
+                    <li>
+                        <a href="#"
+                           onclick="igv.browser.loadTrack({
+                               url: 'https://data.broadinstitute.org/igvdata/test/igv-web/segmented_data_080520.seg.gz',
+                               indexed: false,
+                               isLog: true,
+                               name: 'GBM Copy # (TCGA Broad GDAC)'});">GBM Copy # (TCGA Broad GDAC))</a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                           onclick="igv.browser.loadTrack({
+                               type: 'annotation',
+                               format: 'bed',
+                               url: 'https://data.broadinstitute.org/igvdata/annotations/hg19/dbSnp/snp137.hg19.bed.gz',
+                               indexURL: 'https://data.broadinstitute.org/igvdata/annotations/hg19/dbSnp/snp137.hg19.bed.gz.tbi',
+                               visibilityWindow: 200000,
+                               name: 'dbSNP 137'})">dbSNP 137 (bed tabix)
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                           onclick="igv.browser.loadTrack(
+                               {
+                                   type: 'wig',
+                                   format: 'bigwig',
+                                   url: 'https://s3.amazonaws.com/igv.broadinstitute.org/data/hg19/encode/wgEncodeBroadHistoneGm12878H3k4me3StdSig.bigWig',
+                                   name: 'Gm12878H3k4me3'
+                               })">Encode bigwig
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                           onclick="igv.browser.loadTrack(
+                               {
+                                   type: 'alignment',
+                                   format: 'bam',
+                                   url:'https://1000genomes.s3.amazonaws.com/phase3/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
+                                   name: 'HG02450'
+                               })">1KG Bam (HG02450)
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+        <div class="col-md-6">
+            <div>Click on the Refseq track to add genes:</div>
+            <div id="geneList" width="100%" style="background-color: #d5d5d5">
+
+            </div>
+        </div>
+    </div>
+%{--</nav>--}%
+
+
+%{--try example end--}%
+
+
+
+
+
+
+
+
 <div id="mainVariantDivHolder">
 </div>
 
--- web-app/js/lib/dport/igvInit.js
+++ web-app/js/lib/dport/igvInit.js
@@ -14,19 +14,21 @@ mpgSoftware.igvInit = (function () {
                                 chromosome,
                                 startPosition,
                                 endPosition,
-                                phenotype
+                                phenotype,
+                                limit
                                 ) {
         var igvDiv = document.getElementById(nameOfBase);
         const savedVariable = getVariable('#'+nameOfBase);
-        startPosition = startPosition || 127000000
-        endPosition = endPosition || 128000000
-        chromosome = chromosome || "8"
+        startPosition = startPosition || 41233047;
+        endPosition = endPosition || 41235646;
+        chromosome = chromosome || "17"
         phenotype = phenotype || "T2D"
+        limit = limit || 200
         const args = "?phenotype="+phenotype+
                     "&startPos="+startPosition+
                     "&endPos="+endPosition+
                     "&chromosome="+chromosome+
-                    "&limit=500";
+                    "&limit="+limit;
         var options =
             {
                 genome: "hg38",
@@ -39,44 +41,112 @@ mpgSoftware.igvInit = (function () {
                         "url": "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram",
                         "indexURL": "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram.crai",
                         "format": "cram"
+                    }
+                    ,
+                    {
+                        "name": "Sample BAM",
+                        "url": "https://idea-cdn.s3.eu-west-2.amazonaws.com/out.marked.bam",
+                        "indexURL": "https://idea-cdn.s3.eu-west-2.amazonaws.com/out.marked.bai",
+                        "format": "bam"
                     },
                     {
-                        name: 'GWAS Catalog',
-                        //type: 'annotation',
-
-                        // can we use the old t2d code?
-                        // type: 't2d',
-                         trait: "T2D",
-                        label: "Type 2 Diabetes: DIAGRAM GWAS",
-                        maxLogP: 50,
-                        height: 300,
-                         pvalue: "P-value",
-                        colorScale:  {
-                            thresholds: [5e-8, 5e-4, 0.05],
-                            colors: ["rgb(0,102,51)", "rgb(122,179,23)", "rgb(158,213,76)", "rgb(227,238,249)"]
+                        name: "gnomAD coverage at 10x",
+                        url:  "https://s3.us-east-2.amazonaws.com/ccrs/tracks/gnomad-coverage.bw",
+                        type: "wig",
+                        color: function(value) {
+                            var c;
+                            if (value < 0.5){
+                                c = "#ff8c00"
+                            }
+                            if (value >= 0.5){
+                                c = "#add8e6"
+                            }
+                            return c
                         },
-
-
-                        type: 'gwas',
-                        format: 'gtexgwas',
-
-
-
-                        url: savedVariable['retrieveBottomLineVariants']+args,
-                        //url: savedVariable['storedGWASData'],
-
-                        indexed: false,
-                        color: 'rgb(100,200,200)',
-                        displayMode: 'EXPANDED',
-                        order: Number.MAX_SAFE_INTEGER // 2nd to last track
+                        height: 50,
+                        order: 10,
+                        visibilityWindow: 500000
+                    },
+                    {
+                        url: 'https://s3.us-east-2.amazonaws.com/ccrs/tracks/gerp.bw',
+                        name: 'Genomic Evolutionary Rate Profiling',
+                        type: "wig",
+                        color: function(value) {
+                            var c;
+                            if (value < 1.7){
+                                c = "rgb(0,0,255)"
+                            }
+                            if (value >= 1.7){
+                                c = "rgb(255,0,0)"
+                            }
+                            return c
+                        },
+                        order: 14
                     }
+
+                    // ,
+                    // {
+                    //     name: 'GWAS Catalog',
+                    //     //type: 'annotation',
+                    //
+                    //      trait: "T2D",
+                    //      label: "Type 2 Diabetes: DIAGRAM GWAS",
+                    //      maxLogP: 50,
+                    //      height: 300,
+                    //      pvalue: "P-value",
+                    //      colorScale:  {
+                    //         thresholds: [5e-8, 5e-4, 0.05],
+                    //         colors: ["rgb(0,102,51)", "rgb(122,179,23)", "rgb(158,213,76)", "rgb(227,238,249)"]
+                    //      },
+                    //
+                    //
+                    //     type: 'gwas',
+                    //     format: 'gtexgwas',
+                    //
+                    //
+                    //
+                    //     url: savedVariable['retrieveBottomLineVariants']+args,
+                    //     //url: savedVariable['storedGWASData'],
+                    //
+                    //     indexed: false,
+                    //     color: 'rgb(100,200,200)',
+                    //     displayMode: 'EXPANDED',
+                    //     order: Number.MAX_SAFE_INTEGER // 2nd to last track
+                    // }
                 ]
             };
 
         igv.createBrowser(igvDiv, options)
             .then(function (browser) {
+                igv.browser = browser;
+                var genesInList = {};
                 console.log("Created IGV browser");
+
+                browser.on('trackclick', function (track, popoverData) {
+                    var symbol = null;
+                    if ( ( typeof track !== 'undefined') &&
+                        ( track.name == 'Refseq Genes') &&
+                        ( typeof popoverData !== 'undefined') ){
+                        popoverData.forEach(function (nameValue) {
+                            if (nameValue.name && nameValue.name.toLowerCase() === 'name') {
+                                symbol = nameValue.value;
+                            }
+                            if (symbol && !genesInList[symbol]) {
+                                genesInList[symbol] = true;
+                                $("#geneList").append('<li><a href="https://uswest.ensembl.org/Multi/Search/Results?q=' + symbol + '">' + symbol + '</a></li>');
+                            }
+                        });
+                    }
+
+                    // Prevent default pop-over behavior
+                    return false;
+                });
+
             });
+
+        document.addEventListener("DOMContentLoaded", function () {
+            alert("DOMContentLoaded");
+        });
     }
 
 
